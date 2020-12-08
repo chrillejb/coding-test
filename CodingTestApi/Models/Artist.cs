1ae2eb12b503b7b49481197544d94ee56eac7670
Christoffer Jedbäck <chrille.jb@gmail.com>
2020-12-08 21:54:45 +0100

    Changing JSON names based on specification.

--- CodingTestApi/Models/Artist.cs
+++ CodingTestApi/Models/Artist.cs
@@ -1,9 +1,13 @@
+using System.Text.Json.Serialization;
+
 namespace CodingTestApi.Models
 {
     public class Artist
     {
+        [JsonPropertyName("artist_id")]
         public string Id { get; set; }
 
+        [JsonPropertyName("artist_name")]
         public string Name { get; set; }
     }
 }
